e50ba2b8dad30d599d3a743fa094a53127b9c6d0
ChiragAgg5k <110609663+ChiragAgg5k@users.noreply.github.com>
2024-05-29 22:09:18 +0530

    added shepherd

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -8,6 +8,8 @@ import { howLongAgo } from "~/lib/utils";
 import { Badge } from "~/components/ui/badge";
 import { useInView } from "react-intersection-observer";
 import React, { useEffect, useState } from "react";
+import { Button } from "~/components/ui/button";
+import Shepherd from "shepherd.js";
 
 export default function Posts() {
   const { ref, inView } = useInView();
@@ -27,11 +29,46 @@ export default function Posts() {
     }
   }, [inView]);
 
+  const [tourStarted, setTourStarted] = useState(false);
+
+  const tour = new Shepherd.Tour({
+    useModalOverlay: true,
+  });
+
+  tour.addStep({
+    id: "welcome",
+    text: "Welcome to the Askit! I see you are new here. Let me guide you through the platform.",
+    attachTo: {
+      element: ".top-element",
+      on: "bottom",
+    },
+    classes: "border rounded-xl text-sm p-4 mt-6 z-50 bg-background",
+    buttons: [
+      {
+        text: "Next",
+        action: void tour.next,
+        classes:
+          "px-4 py-2 mt-4 bg-foreground text-background rounded-lg hover:bg-neutral",
+      },
+    ],
+  });
+
   return (
-    <PageWrapper className={`pt-16`}>
+    <PageWrapper className={`top-element pt-16`}>
       <div className={`p-6`}>
         <div className={`mb-6 flex items-center justify-between`}>
           <h1 className={`text-xl font-bold`}>Recent Posts</h1>
+          <Button
+            variant="secondary"
+            className={`border text-sm transition-all duration-300 ease-in-out hover:border-primary`}
+            disabled={tourStarted}
+            onClick={() => {
+              void tour.start();
+              setTourStarted(true);
+            }}
+          >
+            Need help?
+          </Button>
         </div>
         {data ? (
           data.pages.map((page) => (
